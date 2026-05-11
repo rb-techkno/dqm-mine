8133c37e05c536fd229f300ff3f0240891dcbfa6
Rushi <rushikesh.bhoskar@techknomatic.com>
2026-05-11 14:52:01 +0530

    server port changed

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
 import app from "./app.js";
 import { config } from "./config/env.js";
 
-const server = app.listen(config.port, "0.0.0.0", () => {
-  console.log(`DataGuard server running on http://localhost:${config.port}`);
+const port = process.env.PORT || 5000;
+
+const server = app.listen(port, "0.0.0.0", () => {
+  console.log(`DataGuard server running on http://localhost:${port}`);
 });
 
 process.on('SIGTERM', () => {
